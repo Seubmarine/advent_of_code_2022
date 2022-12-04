e7d7acdf0546e14c0649f1634bdfd08ec1a85ab6
Seubmarine <seubmarinepro@gmail.com>
2022-12-04 07:54:32 +0100

    day4 solution 2

--- src/day4.rs
+++ src/day4.rs
@@ -9,7 +9,18 @@ impl Assignement {
 	fn contain(&self, other: &Self) -> bool{
 		self.begin <= other.begin && self.end >= other.end
 	}
+
+	fn not_overlap(&self, other: &Self) -> bool {
+		self.end < other.begin || self.begin > other.end
+	}
+
+	fn overlap(&self, other: &Self) -> bool {
+		!self.not_overlap(other)
+	}
 }
+
+// ..345..
+// 12.....
 
 impl FromStr for Assignement {
 	type Err = ParseIntError;
@@ -24,10 +35,16 @@ pub fn day4(filename : &str) {
 	let file = std::fs::read_to_string(filename).unwrap();
 	let lines = file.as_str().lines();
 
-	let solution_one : u32 = lines.map(|line| line.split_once(',').unwrap())
-	.map(|(l, r)| (l.parse::<Assignement>().unwrap(), r.parse::<Assignement>().unwrap()))
+	let assignements_iter = lines.map(|line| line.split_once(',').unwrap())
+	.map(|(l, r)| (l.parse::<Assignement>().unwrap(), r.parse::<Assignement>().unwrap()));
+	
+	let solution_one : u32 = assignements_iter.clone()
 	.map(|(l, r)| (l.contain(&r) || r.contain(&l)) as u32)
 	.sum();
-
 	println!("Solution 1: {solution_one}");
+
+	let solution_two : u32 = assignements_iter
+	.map(|(l, r)| (l.overlap(&r)) as u32)
+	.sum();
+	println!("Solution 2: {solution_two}");
 }
